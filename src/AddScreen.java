a23fd25e966080559a4aabda66e561b0c2f7f199
chris <chris@DESKTOP-BDLA4EU>
2018-11-11 19:48:20 +0200

    Created Add bew PArticipant Screen

--- src/AddScreen.java
+++ src/AddScreen.java
@@ -3,10 +3,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class AddScreen {
 
-	private JFrame frame;
+	private JFrame frmAddNewParticipant;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -16,7 +25,7 @@ public class AddScreen {
 			public void run() {
 				try {
 					AddScreen window = new AddScreen();
-					window.frame.setVisible(true);
+					window.frmAddNewParticipant.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,13 +44,64 @@ public class AddScreen {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmAddNewParticipant = new JFrame();
+		frmAddNewParticipant.setTitle("Add new Participant");
+		frmAddNewParticipant.setBounds(100, 100, 450, 300);
+		frmAddNewParticipant.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmAddNewParticipant.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Add new Participant form");
-		lblNewLabel.setBounds(109, 10, 179, 23);
-		frame.getContentPane().add(lblNewLabel);
+		JLabel label = new JLabel("Name:");
+		label.setBounds(19, 10, 69, 14);
+		frmAddNewParticipant.getContentPane().add(label);
+		
+		JLabel label_1 = new JLabel("Surname:");
+		label_1.setBounds(19, 34, 69, 14);
+		frmAddNewParticipant.getContentPane().add(label_1);
+		
+		JLabel label_2 = new JLabel("Date Of Birth:");
+		label_2.setBounds(19, 60, 86, 14);
+		frmAddNewParticipant.getContentPane().add(label_2);
+		
+		JLabel label_3 = new JLabel("Telephone:");
+		label_3.setBounds(19, 84, 69, 13);
+		frmAddNewParticipant.getContentPane().add(label_3);
+		
+		JLabel label_4 = new JLabel("Email:");
+		label_4.setBounds(19, 107, 45, 13);
+		frmAddNewParticipant.getContentPane().add(label_4);
+		
+		JRadioButton radioButton = new JRadioButton("Smoker");
+		radioButton.setVerticalAlignment(SwingConstants.TOP);
+		radioButton.setBounds(19, 143, 109, 23);
+		frmAddNewParticipant.getContentPane().add(radioButton);
+		
+		textField = new JTextField();
+		textField.setBounds(110, 8, 145, 19);
+		frmAddNewParticipant.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(110, 32, 145, 19);
+		frmAddNewParticipant.getContentPane().add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(110, 58, 145, 19);
+		frmAddNewParticipant.getContentPane().add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(110, 81, 145, 19);
+		frmAddNewParticipant.getContentPane().add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(110, 104, 145, 19);
+		frmAddNewParticipant.getContentPane().add(textField_4);
+		textField_4.setColumns(10);
+		
+		JButton btnInsertParticipant = new JButton("Insert");
+		btnInsertParticipant.setBounds(174, 185, 81, 21);
+		frmAddNewParticipant.getContentPane().add(btnInsertParticipant);
 	}
 }
