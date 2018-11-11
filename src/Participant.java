9ef5677944cdd1f30b9dde81280137a29f4d46d6
chris <chris@DESKTOP-BDLA4EU>
2018-11-11 21:57:14 +0200

    Fixes

--- src/Participant.java
+++ src/Participant.java
@@ -7,13 +7,11 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JRadioButton;
 import javax.swing.SwingConstants;
-<<<<<<< HEAD
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JMenuBar;
 import javax.swing.JCheckBoxMenuItem;
-=======
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JList;
@@ -26,7 +24,6 @@ import javax.swing.JTextPane;
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
 import java.awt.event.ActionEvent;
->>>>>>> branch 'master' of https://github.com/CS-UCY-EPL343/epl343.winter18.team12.git
 
 public class Participant {
 
@@ -65,71 +62,30 @@ public class Participant {
 		frmParticipant.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmParticipant.getContentPane().setLayout(null);
 		
-		JLabel lblName = new JLabel("Name:");
-<<<<<<< HEAD
-		lblName.setBounds(62, 37, 69, 14);
-		frmParticipant.getContentPane().add(lblName);
-=======
-		lblName.setBounds(61, 70, 109, 14);
-		frame.getContentPane().add(lblName);
->>>>>>> branch 'master' of https://github.com/CS-UCY-EPL343/epl343.winter18.team12.git
-		
-		JLabel lblSurname = new JLabel("Surname:");
-<<<<<<< HEAD
-		lblSurname.setBounds(62, 61, 69, 14);
-		frmParticipant.getContentPane().add(lblSurname);
-=======
-		lblSurname.setBounds(61, 95, 109, 14);
-		frame.getContentPane().add(lblSurname);
->>>>>>> branch 'master' of https://github.com/CS-UCY-EPL343/epl343.winter18.team12.git
-		
-		JLabel lblDateOfBirth = new JLabel("Date Of Birth:");
-<<<<<<< HEAD
-		lblDateOfBirth.setBounds(62, 87, 86, 14);
-		frmParticipant.getContentPane().add(lblDateOfBirth);
-=======
-		lblDateOfBirth.setBounds(61, 120, 109, 14);
-		frame.getContentPane().add(lblDateOfBirth);
->>>>>>> branch 'master' of https://github.com/CS-UCY-EPL343/epl343.winter18.team12.git
-		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Smoker");
 		rdbtnNewRadioButton.setVerticalAlignment(SwingConstants.TOP);
-<<<<<<< HEAD
 		rdbtnNewRadioButton.setBounds(62, 170, 109, 23);
 		frmParticipant.getContentPane().add(rdbtnNewRadioButton);
 		
 		JLabel lblPartname = new JLabel("Part_name");
-		lblPartname.setBounds(171, 38, 76, 13);
+		lblPartname.setBounds(159, 26, 76, 13);
 		frmParticipant.getContentPane().add(lblPartname);
 		
 		JLabel label = new JLabel("Part_name");
-		label.setBounds(171, 63, 76, 13);
+		label.setBounds(159, 50, 76, 13);
 		frmParticipant.getContentPane().add(label);
 		
 		JLabel lblNewLabel = new JLabel("Part_Date");
-		lblNewLabel.setBounds(171, 88, 76, 13);
+		lblNewLabel.setBounds(159, 76, 76, 13);
 		frmParticipant.getContentPane().add(lblNewLabel);
 		
-		JLabel lblTelephone = new JLabel("Telephone:");
-		lblTelephone.setBounds(62, 111, 69, 13);
-		frmParticipant.getContentPane().add(lblTelephone);
-		
 		JLabel lblParttel = new JLabel("Part_Tel");
-		lblParttel.setBounds(171, 111, 69, 13);
+		lblParttel.setBounds(159, 99, 69, 13);
 		frmParticipant.getContentPane().add(lblParttel);
 		
-		JLabel lblEmail = new JLabel("Email:");
-		lblEmail.setBounds(62, 134, 45, 13);
-		frmParticipant.getContentPane().add(lblEmail);
-		
 		JLabel lblPartemail = new JLabel("Part_email");
-		lblPartemail.setBounds(171, 134, 69, 13);
+		lblPartemail.setBounds(159, 122, 69, 13);
 		frmParticipant.getContentPane().add(lblPartemail);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"------------", "hhhh"}));
-		comboBox.setBounds(64, 227, 183, 23);
-		frmParticipant.getContentPane().add(comboBox);
 		
 		JLabel lblFormsCompleted = new JLabel("Forms completed");
 		lblFormsCompleted.setBounds(62, 204, 109, 13);
@@ -146,37 +102,47 @@ public class Participant {
 		JButton btnInsetNewForm = new JButton("Insert new Form");
 		btnInsetNewForm.setBounds(304, 228, 145, 21);
 		frmParticipant.getContentPane().add(btnInsetNewForm);
-=======
 		rdbtnNewRadioButton.setBounds(61, 141, 109, 23);
-		frame.getContentPane().add(rdbtnNewRadioButton);
+		frmParticipant.getContentPane().add(rdbtnNewRadioButton);
+		
+		JLabel label_1 = new JLabel("Email:");
+		label_1.setBounds(61, 122, 45, 13);
+		frmParticipant.getContentPane().add(label_1);
+		
+		JLabel label_2 = new JLabel("Telephone:");
+		label_2.setBounds(61, 99, 69, 13);
+		frmParticipant.getContentPane().add(label_2);
+		
+		JLabel label_3 = new JLabel("Date Of Birth:");
+		label_3.setBounds(61, 75, 86, 14);
+		frmParticipant.getContentPane().add(label_3);
+		
+		JLabel label_4 = new JLabel("Surname:");
+		label_4.setBounds(61, 49, 69, 14);
+		frmParticipant.getContentPane().add(label_4);
+		
+		JLabel label_5 = new JLabel("Name:");
+		label_5.setBounds(61, 25, 69, 14);
+		frmParticipant.getContentPane().add(label_5);
 		
 		JLabel lblParticipantDetails = new JLabel("Participant Details");
 		lblParticipantDetails.setBounds(258, 11, 109, 23);
-		frame.getContentPane().add(lblParticipantDetails);
+		frmParticipant.getContentPane().add(lblParticipantDetails);
 		
 		JButton btnModify = new JButton("Modify");
-		btnModify.setBounds(484, 66, 89, 23);
-		frame.getContentPane().add(btnModify);
-		
-		JLabel lblLabelcombo = new JLabel("Completed Forms");
-		lblLabelcombo.setBounds(412, 208, 132, 14);
-		frame.getContentPane().add(lblLabelcombo);
+		btnModify.setBounds(240, 140, 89, 23);
+		frmParticipant.getContentPane().add(btnModify);
 		
 		String[] formStrings={"Choose Form...", "Form A", "Form B", "Form C", "Form D"};
-		JComboBox comboBox = new JComboBox(formStrings);
-		comboBox.addActionListener(new ActionListener() {
+		JComboBox comboBox1 = new JComboBox(formStrings);
+		comboBox1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			String form=(String)comboBox.getSelectedItem();
-			if(form.equals("Form A"))lblLabelcombo.setText("Completed Forms A");
-			if(form.equals("Form B"))lblLabelcombo.setText("Completed Forms B");
-			if(form.equals("Form C"))lblLabelcombo.setText("Completed Forms C");
-			if(form.equals("Form D"))lblLabelcombo.setText("Completed Forms D");
-			if(form.equals("Choose Form..."))lblLabelcombo.setText("Completed Forms");
+			String form=(String)comboBox1.getSelectedItem();
 			}
 		});
 		
-		comboBox.setBounds(184, 204, 132, 23);
-		frame.getContentPane().add(comboBox);
+		comboBox1.setBounds(61, 227, 132, 23);
+		frmParticipant.getContentPane().add(comboBox1);
 		
 	
 		
@@ -184,6 +150,5 @@ public class Participant {
 
 		
 	
->>>>>>> branch 'master' of https://github.com/CS-UCY-EPL343/epl343.winter18.team12.git
 	}
 }
