6fb283a321b69aaee13c1942f62cea0dbc455d1f
cseas001 <cseas001@b123ws15.cs.ucy.ac.cy>
2018-11-14 18:35:58 +0200

    Now created class Participant

---
+++ src/Participant.java
@@ -0,0 +1,71 @@
+
+public class Participant {
+
+	private String Fname;
+	private String Sname;
+	private String ID;
+	private String DateOfBirth;
+	private String Telephone;
+	private String email;
+	private boolean smoker;
+	//todo Add list of forms
+	public Participant(String first,String sec,String I,String DOB,String Tel,String em,boolean smok) {
+		Fname=first;
+		Sname=sec;
+		ID=I;
+		DateOfBirth=DOB;
+		Telephone=Tel;
+		email=em;
+		smoker=smok;
+	}
+	public void modify(String first,String sec,String I,String DOB,String Tel,String em,boolean smok){
+		Fname=first;
+		Sname=sec;
+		ID=I;
+		DateOfBirth=DOB;
+		Telephone=Tel;
+		email=em;
+		smoker=smok;
+	}
+	public String ret_Fname(){
+		if(LogIn_Screen.is_admin()){
+			return "****";
+		}
+		else{
+			return Fname;
+		}
+	}
+	public String ret_Sname(){
+		if(LogIn_Screen.is_admin()){
+			return "****";
+		}
+		else{
+			return Sname;
+		}
+	}
+	public String ret_Telephone(){
+		if(LogIn_Screen.is_admin()){
+			return "****";
+		}
+		else{
+			return Telephone;
+		}
+	}
+	public String ret_email(){
+		if(LogIn_Screen.is_admin()){
+			return "****";
+		}
+		else{
+			return email;
+		}
+	}
+	public String ret_ID(){
+		return ID;
+	}
+	public String ret_Date(){
+		return DateOfBirth;
+	}
+	public boolean ret_smoker(){
+		return smoker;
+	}
+}
